5456c7693805e2d72250d23dc9d92eb3380b6f24
Cesar Campos <camposss.415@gmail.com>
2023-04-13 15:57:29 -0700

    added new feature on my about section

--- components/About.tsx
+++ components/About.tsx
@@ -27,7 +27,12 @@ function About() {
               Hello, I am a Frontend Software Engineer with a passion for coding
               and creating innovative solutions. When I'm not busy working on my{" "}
               <span className="text-[#46a0ff]">
-                <Link to="projects" spy={true} smooth={true} duration={100} className='cursor-pointer'>
+                <Link
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  duration={100}
+                  className="cursor-pointer">
                   Projects
                 </Link>
               </span>{" "}
@@ -43,7 +48,45 @@ function About() {
             </p>
           </div>
         </div>
-        
+        <div className=" flex flex-col relative group mx-auto md:h-[600px] max-w-[600px]">
+          <div className="md:absolute top-10 left-36 w-auto h-auto m-auto shadow-2xl rounded-xl flex items-center justify-center p-4 md:group-hover:scale-105 ease-in duration-300 flex-col group my-12  md:my-0 md:rotate-[15deg] md:group-hover:translate-x-[160px] md:group-hover:rotate-0 bg-white">
+            <Image
+              src={aboutImg}
+              className="group rounded-md"
+              width={300}
+              height={300}
+              alt="img"
+            />
+            <div className="flex flex-col items-center justify-between my-2">
+              <p className="flex mt-1 pt-8 font-permanentMarker">
+                I love exploring new places{" "}
+              </p>
+            </div>
+          </div>
+          <div className="md:absolute top-10 z-2 left-36 w-auto h-auto m-auto shadow-2xl rounded-xl flex items-center justify-center p-4 md:group-hover:scale-105 ease-in duration-300 flex-col group md:rotate-[-15deg] md:group-hover:translate-x-[-200px] md:group-hover:rotate-0 bg-white">
+            <Image
+              src={babiesImg}
+              className="group rounded-md"
+              width={300}
+              height={300}
+              alt="img"
+            />
+            <div className="flex pt-8 items-center justify-start my-2">
+              <p className="flex mt-1 font-permanentMarker">
+                Buki + Me + Koda{" "}
+                <span className="ml-1 block">
+                  <Image
+                    src={heartImg}
+                    className=""
+                    width={25}
+                    height={25}
+                    alt="img"
+                  />
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
